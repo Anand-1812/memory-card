d454dc3116316e02bfdbfbfaf91ea11ae1e42882
Anand <anand.work2005@gmail.com>
2025-07-22 18:26:20 +0530

    trying to display the pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import extractPokeImage from "./pokeApi"
+import PokemonList from './components/Pokemon.jsx';
 
 function App() {
 
   return (
     <>
       <h1>Memory card</h1>
-      <button onClick={extractPokeImage}>Pokemon</button>
+      <button onClick={PokemonList}>Pokemon</button>
     </>
   )
 }
