0c2c4d21a5eb3334ebb9d0ed0a5e2217ce611abd
Anand <anand.work2005@gmail.com>
2025-07-23 22:59:36 +0530

    wrap the app in mainDiv:

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ function App() {
 
   return (
     <>
-      <div className="navBar">
-        <h1>Memory card</h1>
-        <button onClick={() => setShowList(true)}>Get Pokemon</button>
+      <div className="mainDiv">
+        <div className="navBar">
+          <h1>Memory card</h1>
+          <button onClick={() => setShowList(true)}>Get Pokemon</button>
+        </div>
+        {showList && <PokemonList />}  
       </div>
-      {showList && <PokemonList />}  
     </>
   );
 }
