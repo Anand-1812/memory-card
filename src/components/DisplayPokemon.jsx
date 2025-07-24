56bf73269dd8b85d6d4e277638ec05ac3ffe7eac
Anand <anand.work2005@gmail.com>
2025-07-24 16:44:29 +0530

    add the onClick method to all card

--- src/components/DisplayPokemon.jsx
+++ src/components/DisplayPokemon.jsx
@@ -1,7 +1,7 @@
 function DisplayPokemon({ name, imageUrl }) {
   return (
     <>
-      <div className="card">
+      <div className="card" onClick={() => console.log("clicked " + name)} >
         <img src={imageUrl} alt={name} />
         <p>{name}</p>
       </div>
