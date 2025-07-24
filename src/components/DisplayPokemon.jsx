45b806734aa5c3aadc3a678add36118d79ca9eac
Anand <anand.work2005@gmail.com>
2025-07-24 17:32:14 +0530

    add the score and max score functionality just took help in how to keep count of clicked pokemons

--- src/components/DisplayPokemon.jsx
+++ src/components/DisplayPokemon.jsx
@@ -1,12 +1,24 @@
-export function increaseScore(score, setScore) {
-  setScore(score+1);
+export function increaseScore(name, score, setScore, maxScore, setMaxScore, clickedPokemon, setClickedPokemon) {
+  if (!clickedPokemon.includes(name)) {
+    setScore(score + 1);
+    setClickedPokemon([...clickedPokemon, name]);
+  } else {
+    setMaxScore(Math.max(score, maxScore));
+    setScore(0);
+    setClickedPokemon([]);
+  }
 }
 
-function DisplayPokemon({ name, imageUrl, score, setScore }) {
+function DisplayPokemon({
+  name, imageUrl, 
+  score, setScore, 
+  maxScore, setMaxScore,
+  clickedPokemon, setClickedPokemon
+}) {
 
   return (
     <>
-      <div className="card" onClick={() => increaseScore(score, setScore)} >
+      <div className="card" onClick={() => increaseScore(name, score, setScore, maxScore, setMaxScore, clickedPokemon, setClickedPokemon)} >
         <img src={imageUrl} alt={name} />
         <p>{name}</p>
       </div>
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -3,7 +3,13 @@ import DisplayPokemon from "./DisplayPokemon";
 
 const POKEMON = ['pikachu', 'squirtle', 'charmander', 'bulbasaur', 'charizard', 'ditto', 'scizor', 'eevee', 'gengar', 'mew'];
 
-function PokemonList({ pokemonList, setPokemonList, score, setScore }) {
+function PokemonList({ 
+  pokemonList, 
+  setPokemonList, 
+  score, setScore, 
+  maxScore, setMaxScore, 
+  clickedPokemon, setClickedPokemon
+}) {
 
   useEffect(() => {
     async function fetchPokeImage() {
@@ -11,14 +17,14 @@ function PokemonList({ pokemonList, setPokemonList, score, setScore }) {
         const responses = await Promise.all(
           POKEMON.map(pokemon =>
             fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-              .then(res => {
-                if (!res.ok) throw new Error(`HTTP error for ${pokemon}`);
-                return res.json();
-              })
-              .then(data => ({
-                name: pokemon,
-                imageUrl: data.sprites.front_default
-              }))
+            .then(res => {
+              if (!res.ok) throw new Error(`HTTP error for ${pokemon}`);
+              return res.json();
+            })
+            .then(data => ({
+              name: pokemon,
+              imageUrl: data.sprites.front_default
+            }))
           )
         );
 
@@ -34,7 +40,11 @@ function PokemonList({ pokemonList, setPokemonList, score, setScore }) {
   return (
     <div className="cardDiv">
       {pokemonList.map(p => (
-        <DisplayPokemon key={p.name} name={p.name} imageUrl={p.imageUrl} score={score} setScore={setScore} />
+        <DisplayPokemon key={p.name} name={p.name} imageUrl={p.imageUrl} 
+          score={score} setScore={setScore} 
+          maxScore={maxScore} setMaxScore={setMaxScore}
+          clickedPokemon={clickedPokemon} setClickedPokemon={setClickedPokemon}
+        />
       ))}
     </div>
   );
